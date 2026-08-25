2badddca8206a43ffec6a3322f481e71e1e11c71
Oyabure-Zakari <oyabure205@gmail.com>
2026-08-25 14:22:12 +0100

    feat: implement ChatGroq LLM initialization and invocation in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-import "dotenv/config";
+import { ChatGroq } from '@langchain/groq';
+import 'dotenv/config';
 
-console.log("Hello, World!");
+
+console.log(process.env.GROQ_API_KEY);
+
+// Initialize the LLM
+const llm = new ChatGroq({
+  apiKey: process.env.GROQ_API_KEY || "",
+  model: "compound-beta",
+  temperature: 0.7,
+  maxTokens: 1000,
+});
+
+// Run the LLM
+const result = await llm.invoke('What is the capital of France?');
+console.log(result.content);
