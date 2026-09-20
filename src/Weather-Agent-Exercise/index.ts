84aa4cd2513793293108f2242578f1071d4d5617
Oyabure-Zakari <oyabure205@gmail.com>
2026-09-20 10:16:10 +0100

    feat: enhance weather tool to return detailed weather information and integrate with LLM graph

--- src/Weather-Agent-Exercise/index.ts
+++ src/Weather-Agent-Exercise/index.ts
@@ -1,8 +1,11 @@
 import { ChatGroq } from "@langchain/groq";
 import "dotenv/config";
-import { MessagesValue, StateSchema } from "@langchain/langgraph";
+import { MessagesValue, StateGraph, StateSchema, type GraphNode } from "@langchain/langgraph";
 import { tool } from "@langchain/core/tools";
 import * as z from "zod";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { InMemoryCache } from "@langchain/langgraph-checkpoint";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 
 // Global variables
 const userPrompt = "How is the weather in Abuja?";
@@ -46,7 +49,16 @@ const getCurrentWeatherData = async (location: string) => {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
+    return `
+      Weather in ${data.location.name}, ${data.location.country}:
+      Condition: ${data.current.condition.text}
+      Temperature: ${data.current.temp_c}°C (feels like ${data.current.feelslike_c}°C)
+      Chance of rain: ${data.current.chance_of_rain}%
+      Humidity: ${data.current.humidity}%
+      Wind: ${data.current.wind_kph} km/h ${data.current.wind_dir}
+      Visibility: ${data.current.vis_km} km
+      Last updated: ${data.current.last_updated}
+    `;
   } catch (error: unknown) {
     throw new Error(`Fetch failed: ${(error as Error).message}`);
   }
@@ -69,8 +81,28 @@ const weatherTool = tool(
 // Bind the LLM with tools
 const llmWithTools = llm.bindTools([weatherTool]);
 
-// Invoke the LLM with input that triggers the tool call
-const msg = await llmWithTools.invoke("What is the weather in Toronto?");
+// Nodes
+// This node calls the llm which decides whether to call a tool or not.
+const llmCall: GraphNode<typeof State> = async (state) => {
+  const response = await llmWithTools.invoke([new SystemMessage(systemPrompt), ...state.messages]);
+  return {
+    messages: [response],
+  };
+};
+// The tool node is used to call the tools and return the results.
+const toolNode = new ToolNode([weatherTool]);
 
-// Get the tool call
-console.log(msg.tool_calls);
+// Build the graph
+const graph = new StateGraph(State)
+  .addNode("llmNode", llmCall, { cachePolicy: { ttl: 300 } })
+  .addNode("toolNode", toolNode, { cachePolicy: { ttl: 300 } })
+  .addEdge("__start__", "llmNode")
+  .addEdge("llmNode", "toolNode")
+  .compile({ cache: new InMemoryCache() });
+
+// Run the graph
+const response = await graph.invoke({
+  messages: [new HumanMessage(userPrompt)],
+});
+
+console.log(response.messages[2]?.content);
